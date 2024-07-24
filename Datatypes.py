e34833c43de4b876ccbacdaf83bd6fc1f1598605
Modukuri-Thanuja <128662892+Modukuri-Thanuja@users.noreply.github.com>
2024-07-24 09:34:29 +0530

    Day-1

---
+++ Datatypes.py
@@ -0,0 +1,30 @@
+"""Int
+float
+string
+boolean
+set 
+tuple
+dictionary
+list
+complex
+print("Hello Swec")
+var="python"
+print("good morning",var)
+name=input("enter your name\n")
+print("hello",name)
+name1=input("Enter the name")
+age=int(input())
+print(f"Hello {name1} you are {age} years old")
+name=input()
+s1=int(input())
+s2=int(input())
+s3=int(input())
+s4=int(input())
+s5=int(input())
+print(f"your name is {name} and your tptal marks are {sum} and avg is {avg})
+name=input()
+age=int(input())
+if age >=18:
+    print(f"hello {name} your age is {age} you are eligible for voting")
+elif age<18:
+    print(f"hello {name} your age is {age} you are not eligible for voting")"""
---
+++ pythonDay-1-problem_3.py
@@ -0,0 +1,61 @@
+"""a=int(input())
+b=int(input())
+#print(a+b,a-b,a//b,a%b,a*b,a**b)
+print(a**b)
+#logical operators(and,or)
+age=int(input())
+if age>=18 and age<24:
+    print("only two wheeler")
+elif age>24 and age<45:
+    print("two wheeler and four wheeler")
+else:
+    print("all the vehicles")1
+10 apples-15
+24 bananas-4
+8 oranges-5
+n=int(input())
+a,b,c=map(int,input().split())
+app=a
+ban=b
+org=c
+tot=app+ban+org
+if(tot<n):
+    print("The shopkeeper not cheated")
+elif(tot>n):
+    print("the shopkeeper is cheated")
+a=int(input())
+if (a>0 and a%2==0):
+    print("the number is positive and even")
+elif (a>0 and a%2!=0):
+    print("the number is positive and odd")
+elif (a<0 and a%2==0):
+    print("the number is negative and even")
+elif (a<0 and a%2!=0):
+    print("the number is negative and odd")
+"mr z  is selected for olympics he is participating
+in swimming competition only 1 winner is selcted in the competition and
+mr x $mr y are frnds of z mr x is in badminton and y in table tennis acoording to the selection commitee the reqs for badminton
+pllayers are 1.height:140cm 2.weight is factors of 2 bdy fat
+<12
+accord to selection commitee req for tennis are height min 118 cm to 148 cm,weight:factors of no of medals won by mr z ,
+    body fat:14% accor to prev history z participated in 14 games out of which he is having success rate of 50%
+write a prog to check whether mr x y z may travel in a same plane or Not from india 
+x,y=map(int,input().split())
+x_height=int(input())
+x_weight=int(input())
+y_height=int(input())
+y_weight=int(input())
+if(x_height=140 and x_weight%2==0):
+    print(" x is selected")
+else:
+    print(x is not selected)
+if(y_height>118 and y_height<=148 and y_weight%7==0):
+    print("y is selected")
+else:
+    print(y is not selected)
+if():
+    print("all will go in same plane")
+else:
+    print("they will not go")"""
+
+
